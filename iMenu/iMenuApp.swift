23dde11c47983b8292db76bae4f5615a23f2090f
Yusuf Demirci <yusuf@nefarius.co>
2026-07-10 10:25:53 +0300

    Initial Commit

---
+++ iMenu/iMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMenuApp.swift
+//  iMenu
+//
+//  Created by Yusuf Demirci on 10.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct iMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iMenuTests/iMenuTests.swift
@@ -0,0 +1,19 @@
+//
+//  iMenuTests.swift
+//  iMenuTests
+//
+//  Created by Yusuf Demirci on 10.07.2026.
+//
+
+import Testing
+@testable import iMenu
+
+struct iMenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
